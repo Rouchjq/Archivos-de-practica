4557d8bb414239f639e109ce3183e096f57195ce
Jorge Quinero <rouchjq8@gmail.com>
2021-04-23 01:14:59 -0400

    guardado de practica 1

---
+++ Javascript/js/introduccion.js
@@ -0,0 +1,17 @@
+/* COMENTARIO MULTI LINEA*/
+
+//alerta en js
+
+// alert("hola wapo");
+// alert("hola wapo 2.0");
+
+//CONSOLA EN JAVASCRIPT
+/*console.log("Hola, saludamos a la consola");
+console.log(2+2);
+console.log(8*4);
+console.info("Mostrando una informacion");
+console.warn("esto es una advertencia");
+console.error("Esto es un error");*/
+
+//DOCUMENT WRITE para escribir en pantalla
+document.write("HOLA este mensaje se mostrar en mi pagina");
